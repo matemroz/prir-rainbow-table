dc524839fdcde149b4cf4c2af4a434c29cb4bca3
matemroz@gmail.com <matemroz@gmail.com@b251ac84-4d47-869a-6ba3-4a01878d2b80>
2011-06-27 19:57:58 +0000

    quicksort

--- trunk/RainbowTable/sorter.h
+++ trunk/RainbowTable/sorter.h
@@ -1,3 +1,5 @@
+void quicksort(char*** tab, int p, int k);
+int merge(char*** tab, int p, int k);
 void swap(char **str1, char **str2);
 
-void quicksort(char* rainbowTable[][2], int l, int r);
+//void quicksort(char*** rainbowTable, int l, int r);
