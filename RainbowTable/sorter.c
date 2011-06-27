9c839ca82346794c2c2fcc5659e708f3a49ad369
matemroz <matemroz@gmail.com>
2011-06-27 19:57:58 +0000

    quicksort

--- RainbowTable/sorter.c
+++ RainbowTable/sorter.c
@@ -5,23 +5,43 @@
 #include "sorter.h"
 #include "rainbow.h"
 
+void quicksort(char*** tab, int p, int k) {
+    if (p < k) {
+        int q = merge(tab, p, k);
+        quicksort(tab, p, q - 1);
+        quicksort(tab, q + 1, k);
+    }
+}
+
+int merge(char*** tab, int p, int k) {
+    int i;
+    int j;
+    
+    char *x = (char *) malloc(DES_CHARS_NUM * sizeof (char));
+    x = (char *) tab[k][1];
+    i = p - 1;
+
+    for (j = p; j < k; j++) {
+        if(strcmp(tab[j][1], x) <= 0)
+        {
+            i++;
+            swap(&tab[i][0], &tab[j][0]);
+            swap(&tab[i][1], &tab[j][1]);
+        }
+    }
+    swap(&tab[i + 1][0], &tab[k][0]);
+    swap(&tab[i + 1][1], &tab[k][1]);
+    
+    return i + 1;
+}
+
 void swap(char **str1, char **str2) {
     char *temp = *str1;
     *str1 = *str2;
     *str2 = temp;
 }
-
-
-/* UŻYCIE quicksorta:
-   char *tab[3][2] = {
-        {"jaifd", "blablablabl"},
-        {"fafaf", "iahdfoahafh"},
-        {"fafaf", "iahdfoahafh"}
-    };
-
-    quicksort(tab, 0, 3);
-*/
-void quicksort(char* rainbowTable[][2], int l, int r) {
+/*
+void quicksort(char*** rainbowTable, int l, int r) {
 
 
     int i = l;
@@ -50,3 +70,4 @@ void quicksort(char* rainbowTable[][2], int l, int r) {
     if (i < r)
         quicksort(rainbowTable, i, r);
 }
+*/
