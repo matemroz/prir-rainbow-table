167a95fcdbeba858479af77fa9c5342b2ec703dc
matemroz <matemroz@gmail.com>
2011-06-28 18:15:01 +0000

    dodane pliki do projektu: file_writer, formatter, uaktualniony Makefile

---
+++ RainbowTable/formatter.c
@@ -0,0 +1,15 @@
+int str_to_int(char *str, int n) {
+    int str_int;
+    int i;
+
+    str_int = 0;
+    i = 0;
+
+    for (i = 0; i < n; i++) {
+
+        char c = *(str + i);
+        str_int += (int) c;
+    }
+
+    return str_int;
+}
