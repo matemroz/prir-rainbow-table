8474a4fe5bab6c2b7707a0e07639d0eafcc92b10
matemroz <matemroz@gmail.com>
2011-06-29 06:23:37 +0000

    argumenty

--- RainbowTable/main.c
+++ RainbowTable/main.c
@@ -12,13 +12,14 @@
 int main(int argc, char *argv[]) {
 
 
-    /* BLOK DEKLARACJI ZMIENNYCH */
 
+    /* BLOK DEKLARACJI ZMIENNYCH */
     int rank; /* numer procesu */
     int size; /* ilosc procesow */
     int depth = 5; /* glebokosc lancucha teczowego */
     int passSize = 10; /* dlugosc hasla */
-    int passType = 0; /* rodzaj hasla */
+    int passType = 0; /*zbior znakow*/
+    char *hash_rt;
     char **passTab;
     char ***rainbowTab;
     int passCount = 0;
@@ -28,6 +29,72 @@ int main(int argc, char *argv[]) {
     int src = 0;
     int i = 0;
     char * msgStream;
+    char *filename_rt;
+
+    /*Generowanie tablicy teczowej*/
+    if (argc > 1 && strcmp(argv[1], "-gen") == 0) {
+        if (strcmp(argv[2], "-n") == 0 && strcmp(argv[4], "-p") == 0 && strcmp(argv[6], "-t") == 0 && strcmp(argv[8], "-d") == 0 && strcmp(argv[10], "-o") == 0) {
+            size = atoi(argv[3]); //liczba procesow
+            passSize = atoi(argv[5]); //dlugosc lancucha
+            passType = atoi(argv[7]); //typ znakow
+            depth = atoi(argv[9]); //glebokosc
+            filename_rt = argv[11]; //nazwa pliku z tablica teczowa
+        } else {
+            fprintf(stdout, "Niepoprawne parametry! Uruchom program z opcja '-h', zeby zobaczyc pomoc dla programu"
+                    " lub '-u', zeby zobaczyc uzycie.");
+            return -1;
+        }
+    }       /*Lamanie hasla*/
+    else if (argc > 1 && strcmp(argv[1], "-b") == 0) {
+        if (strcmp(argv[2],"-cr") == 0 && strcmp(argv[4],"-r") == 0) {
+            hash_rt = argv[3];  //TODO: wykorzystanie hasha
+            filename_rt = argv[5];
+        } else {
+            fprintf(stdout, "Niepoprawne parametry! Uruchom program z opcja '-h', zeby zobaczyc pomoc dla programu"
+                    " lub '-u', zeby zobaczyc uzycie.");
+            return -1;
+        }
+    }        /*Wyswietlenie pomocy*/
+    else if (argc > 1 && strcmp(argv[1], "-h") == 0) {
+        fprintf(stdout, "Dzialanie programu oparte jest na wykorzystaniu tablic teczowych. W programie"
+                " istnieje mozliwosc wygenerowania tablicy zgodnie z parametrami jakie oczekujemy:\n"
+                "-gen - tryb generowania lancuchow tablicy teczowej'\n"
+                "-n <liczba procesow> - okreslenie liczby procesow\n"
+                "-p <dlugosc hasla> - dlugosc hasla\n"
+                "-t <typ znakow> - typ znakow (0 - alphanum, 1 - bigalphanum, 2 - smallalphanum, 3 - alpha,\n"
+                "4 - smallalpha, 5 - bigalpha, 6 - num, 7 - ext_alphanum).\n"
+                "-d <glebokosc> - okreslenie glebokosci tablicy teczowej.\n"
+                "-o mazwa pliku wyjsciowego z tablica teczowa.\n\n"
+                "W programie istnieje rowniez mozliwosc lamania hasel:\n"
+                "-cr <hash> hash, ktory ma zostac zlamany,\n"
+                "-r <nazwa pliku> plik z tablica teczowa, ktory zostanie wczytany do programu.\n"
+                );
+    }/*Wyswietlenie sposobu uzycia programu*/
+    else if (argc > 1 && strcmp(argv[1], "-u") == 0) {
+        fprintf(stdout, "Generowanie tablicy:\n"
+                "rtg -gen -n <liczba procesow> -p <dlugosc hasla> -t <typ znakow> -d <glebokosc> -r <nazwa pliku>\n\n");
+
+        fprintf(stdout, "Lamanie hasla:\n"
+                "rtg -b -cr <hash> -r <nazwa pliku>\n\n");
+
+        fprintf(stdout, "Wyswietlenie pomocy:\n"
+                "rtg -h\n\n");
+
+        fprintf(stdout, "Uzycie programu:\n"
+                "rtg -u\n\n");
+    } else {
+        fprintf(stdout, "Generowanie tablicy:\n"
+                "rtg -gen -n <liczba procesow> -p <dlugosc hasla> -t <typ znakow> -d <glebokosc> -r <nazwa pliku>\n\n");
+
+        fprintf(stdout, "Lamanie hasla:\n"
+                "rtg -b -cr <hash> -r <nazwa pliku>\n\n");
+
+        fprintf(stdout, "Wyswietlenie pomocy:\n"
+                "rtg -h\n\n");
+
+        fprintf(stdout, "Uzycie programu:\n"
+                "rtg -u\n\n");
+    }
 
 
     //char *tab[28] = {"asdas", "fjgja", "xijff", "wijff", "eijff", "oqijff", "yeyjff", "ieqief", "iqiff", "ssadaf", "yeyjff", "ieqief", "iqiff", "ssadaf","asdas", "fjgja", "xijff", "wijff", "eijff", "oqijff", "yeyjff", "ieqief", "iqiff", "ssadaf", "yeyjff", "ieqief", "iqiff", "ssadaf"};
